8a94206b9c20dfd3ac55aa4986e66e80c210e27b
Allison <hughesneal88@gmail.com>
2024-06-01 00:56:49 +0000

    Updated the header styles

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView>
+      <StatusBar style="auto" />
       <ScrollView>
         <View style={styles.container}>
           <View style={styles.headersectionContainer}>
+            <View style={styles.headersectionHeaderContainer}>
             <View style={styles.headersectionTitleContainer}>
               <Text style={styles.headersectionTitle}>Hello, Devs!</Text>
             </View>
-            <Image source={require('./assets/person.png')} style={styles.headersectionImage} />
+            <Text style={styles.headersectionDescription}>14 tasks today</Text>
+            </View>
+            <View style={[styles.headersectionImageContainer, {borderRadius: 50}]}>
+              <Image source={require('./assets/person.png')} style={styles.headersectionImage} />
+            </View>
           </View>
-          <Text style={styles.headersectionDescription}>14 tasks today</Text>
+        <View style={styles.searchBarContainer}>
+          <View style={styles.searchBar}>
+            <TextInput></TextInput>
+          </View>
         </View>
+        <View style={styles.taskContainer}>
+          <View style={styles.taskHeaderContainer}>
+            {/* <Text style={styles.taskHeader}>Ongoing Tasks</Text> */}
+          </View>
+        </View>
+      </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -21,34 +36,41 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 60,
+
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F7F0E8',
     alignItems: 'center',
     justifyContent: 'center',
-    width: '100%',
-    height: '100%'
+    maxWidth: '100%',
+    maxHeight: '100%'
   },
   headersectionTitleContainer: {
     flex: 1,
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'flex-start',
-
+    alignItems: 'center',
   },
   headersectionContainer: {
+    marginTop: 60,
     paddingHorizontal: 24,
     borderColor: 'black',
     borderCurve: 'round',
     borderWidth: 1,
     borderRadius: 10,
     padding: 5,
-    backgroundColor: 'white',
+    backgroundColor: '#F7F0E8',
     width: "90%",
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+  },
+  headersectionHeaderContainer:{
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    width: 150,
   },
   headersectionTitle: {
     fontSize: 24,
@@ -60,9 +82,22 @@ const styles = StyleSheet.create({
     marginTop: 8,
     fontSize: 12,
     fontWeight: '400',
-    alignItems: 'left',
+    textAlign: 'left',
   },
   headersectionImage: {
+      width: 50,
+      height: 50,
+      alignItems: 'center',
+      
+      textAlign: 'right',
+    },
+    headersectionImageContainer: {
+      width: 100,
+      height: 100,
+      borderRadius: 50,
+      backgroundColor:'white',
+      overflow: 'hidden',
+      marginLeft: 24,
       width: 50,
       height: 50,
       alignItems: 'center',
